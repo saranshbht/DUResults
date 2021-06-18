358306daa0627e632233b3d6e9d7ad12a1041681
Saransh Bhatia <saranshbht@gmail.com>
2021-06-19 02:31:24 +0530

    switched from axios to urllib; added pg-comp-sci results

--- runner.js
+++ runner.js
@@ -1,32 +1,37 @@
-const req = require('./requirements');
+const req = require("./requirements");
 
-const cookie_url = "http://durslt.du.ac.in/DURSLT_MJ2020/Students/Combine_GradeCard.aspx"
-const declared_url = "http://durslt.du.ac.in/DURSLT_MJ2020/Students/List_Of_Declared_Results.aspx"
-const grade_url = "http://durslt.du.ac.in/DURSLT_MJ2020/Students/Combine_GradeCardReport_CBCS.aspx"
-const marks_url = "http://durslt.du.ac.in/DURSLT_MJ2020/Students/OLD_Sys1_GradeCardReport_Sem_New.aspx"
+const cookie_url =
+  "http://durslt.du.ac.in/DURSLT_ND2020/Students/Combine_GradeCard.aspx";
+const declared_url =
+  "http://durslt.du.ac.in/DURSLT_ND2020/Students/List_Of_Declared_Results.aspx";
+const grade_url =
+  "http://durslt.du.ac.in/DURSLT_ND2020/Students/Combine_GradeCardReport_CBCS.aspx";
+const marks_url =
+  "http://durslt.du.ac.in/DURSLT_ND2020/Students/OLD_Sys1_GradeCardReport_Sem_New.aspx";
 
 type = process.argv[2];
-let url = type == 'Marks' ? marks_url : grade_url;
+let url = type == "Marks" ? marks_url : grade_url;
 let step = 10;
-let source = 'html';
-let store = 'json';
-
+let source = "html";
+let store = "json";
 
 console.log(new Date().toISOString());
 console.log(type);
 
 req.loadFiles(type);
-Promise.all([req.getRemaining(declared_url, type), req.setCookie(cookie_url)])
-.then(async([remaining, ]) => {
-	console.log(remaining);
-	// console.log('downloading');
-	await req.downloadHtmls(remaining, type, url, step, source);
-	let to_be_updated = req.toBeUpdated();
-	console.log(to_be_updated);
-	// console.log('making jsons');
-	req.makeJsons(to_be_updated, type, source, store);
-	// console.log('making json-gzips');
-	req.jsonsToJsonGzips(to_be_updated, type, store);
-	// console.log('making csvs');
-	req.jsonsToCsvs(to_be_updated, type, store);
+Promise.all([
+  req.getRemaining(declared_url, type),
+  req.setCookie(cookie_url),
+]).then(async ([remaining]) => {
+  console.log(remaining);
+  // console.log("downloading");
+  await req.downloadHtmls(remaining, type, url, step, source);
+  let to_be_updated = req.toBeUpdated();
+  console.log(to_be_updated);
+  // console.log('making jsons');
+  req.makeJsons(to_be_updated, type, source, store);
+  // console.log('making json-gzips');
+  req.jsonsToJsonGzips(to_be_updated, type, store);
+  // console.log('making csvs');
+  req.jsonsToCsvs(to_be_updated, type, store);
 });
